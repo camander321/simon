7b6122eaef451f5f9b6b3172510c489d6301200b
Anna-Marie Cooper and Cameron Anderson <anna-marie@>
2018-03-12 14:01:18 -0700

    make simon meaner.

--- src/main.js
+++ src/main.js
@@ -2,58 +2,65 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { Simon } from './simon';
 
+function disableButtons(boolean){
+  $('button.red').prop('disabled', boolean);
+  $('button.yellow').prop('disabled', boolean);
+  $('button.green').prop('disabled', boolean);
+  $('button.blue').prop('disabled', boolean);
+}
+
+function meanOutput(color, simon){
+  if (!simon.checkInput(color)) {
+    $('#output').text("Try not to be so awful next time.");
+    disableButtons(true);
+  }
+  else if (simon.index === 0)
+    showColors(simon);
+}
+
 
 function showColors(simon) {
-  var id = setInterval(frame, 1000);
+  var id = setInterval(frame, 500 + 500/simon.colors.length);
   var count = 0;
-
+  disableButtons(true);
   function frame() {
     var jumbo = $('.jumbotron');
     jumbo.removeClass();
     jumbo.addClass("jumbotron");
-    if (count === simon.colors.length) {
+    if (count === simon.colors.length * 2) {
       clearInterval(id);
+      disableButtons(false);
     }
-    else {
-      jumbo.addClass(simon.colors[count]);
-      count++;
+    else if (count % 2 == 0) {
+      jumbo.addClass(simon.colors[count / 2]);
     }
+    count++;
   }
 }
 
-
 $(document).ready(function(){
-
-  var simon = new Simon();
-  simon.newRound();
-  showColors(simon);
+  var simon;
+  disableButtons(true);
+  $('#play').click(function(){
+    simon = new Simon();
+    simon.newRound();
+    showColors(simon);
+  });
 
   $('button.red').click(function(){
-    console.log(simon.checkInput('red'));
-    if (simon.index === 0) {
-      showColors(simon);
-    }
+    meanOutput('red', simon);
   });
 
   $('button.blue').click(function(){
-    console.log(simon.checkInput('blue'));
-    if (simon.index === 0) {
-      showColors(simon);
-    }
+    meanOutput('blue', simon);
   });
 
   $('button.green').click(function(){
-    console.log(simon.checkInput('green'));
-    if (simon.index === 0) {
-      showColors(simon);
-    }
+    meanOutput('green', simon);
   });
 
   $('button.yellow').click(function(){
-    console.log(simon.checkInput('yellow'));
-    if (simon.index === 0) {
-      showColors(simon);
-    }
+    meanOutput('yellow', simon);
   });
 
 });
